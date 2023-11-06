98c805494ebb170c3b7d65033d12970db6864294
Johanna Magdalena <69107842+johannamag@users.noreply.github.com>
2023-11-06 09:56:25 +0100

    setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { navItems as data } from '../data'
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+        Hallo
+    </div>
+  )
+}
